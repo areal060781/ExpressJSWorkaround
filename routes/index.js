68784c17fa9c348da04cc468d08119901aeaac1b
Aida Real <areal060781@hotmail.com>
2015-12-21 08:28:16 -0500

    Ajax search with request module to process the YQL URL

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 //var TinyURL = require('tinyurl');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -59,5 +60,33 @@ router.post('/adduser', function(req, res){
   console.log(res);
 });*/
 
+/*AJAX search form*/
+router.get('/search', function(req, res){
+  res.render('search');
+});
+
+/*searchin method*/
+router.get('/searching', function(req, res){
+  var val = req.query.search;
+  var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
+"%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
+"json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+//resquest module is used to process the yql url and return the results in JSON format
+request(url, function(err, resp, body){
+  body = JSON.parse(body);
+  //logic used to compare search results with the input from user
+  if (!body.query.results.RDF.item){
+    results = "No se encontraron resultados. Intente de nuevo";
+  }else{
+    results = body.query.results.RDF.item[0]['description'];
+  }
+  //pass back the result to client side
+  res.send(results);
+});
+
+  //testing the route
+  //res.send("WHEEE");
+});
 
 module.exports = router;
